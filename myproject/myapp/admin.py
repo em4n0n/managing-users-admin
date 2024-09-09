3204790f0c9adc758a69523f326245b4a7bf826b
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-09 15:05:27 -0700

    Register own admin using the @admin.register decorator

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
-from django.contrib.auth.models import User
+
 # Unregister the provided model admin:
+
 admin.site.unregister(User)
+@admin.register(User)
+class NewAdmin(UserAdmin):
+    pass
