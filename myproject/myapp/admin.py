bbd75b28c0b9bf390dd4dbd305938ae82fbc2298
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-09 20:49:26 -0700

    Disable username for non-superusers

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -11,3 +11,13 @@ class NewAdmin(UserAdmin):
     readonly_fields = [
         'date_joined',
     ]
+    
+def get_form(self, request, obj=None, **kwargs):
+    form = super().get_form(request, obj, **kwargs)
+    is_superuser = request.user.is_superuser
+    
+    if not is_superuser:
+        form.base_fields['username'].disabled = True
+        
+        return form
+    
