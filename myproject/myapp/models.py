7286dac2628428b63f001956d7162f374561b49b
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-11 21:41:54 -0700

    Create a string representation of Persons

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Person(models.Model):
     last_name = models.TextField()
     first_name = models.TextField()
+    
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
